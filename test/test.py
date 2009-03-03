86b2508913a543028d38ce05c56eceda20d30508
Dustin Sallings <dustin@spy.net>
2009-03-03 14:03:32 -0800

    More common file location stuff.

--- test/test.py
+++ test/test.py
@@ -11,18 +11,18 @@ import longurl
 class ParsingTest(unittest.TestCase):
 
     def __parse(self, fn, parser):
-        with open(fn) as f:
+        with open("../test/" + fn) as f:
             return parser(f.read())
 
     def testServiceList(self):
-        s = self.__parse("../test/services.xml", longurl.Services)
+        s = self.__parse("services.xml", longurl.Services)
         self.assertEquals(109, len(s))
         self.assertEquals('snipurl.com', s['snipurl.com'].name)
         urls = set(['snipurl.com', 'snurl.com', 'snipr.com', 'sn.im'])
         self.assertEquals(urls, set(s['snipurl.com'].domains))
 
     def testExpandedURL(self):
-        s = self.__parse("../test/expand.xml", longurl.ExpandedURL)
+        s = self.__parse("expand.xml", longurl.ExpandedURL)
         self.assertEquals('Invalid Item', s.title)
         self.assertEquals('http://cgi.ebay.com/aw-cgi/eBayISAPI.dll?ViewItem&item=1698262135',
                           s.url)
