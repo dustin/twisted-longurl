08664e7005f6ba3055b04c24833189d64f0b4390
Dustin Sallings <dustin@spy.net>
2009-03-02 20:04:07 -0800

    Refactored test stuff.

--- test/test.py
+++ test/test.py
@@ -10,13 +10,16 @@ import longurl
 
 class ParsingTest(unittest.TestCase):
 
+    def __parse(self, fn, parser):
+        with open(fn) as f:
+            return parser(f.read())
+
     def testServiceList(self):
-        with open("test/services.xml") as f:
-            s = longurl.Services(f.read())
-            self.assertEquals(109, len(s))
-            self.assertEquals('snipurl.com', s['snipurl.com'].name)
-            urls = set(['snipurl.com', 'snurl.com', 'snipr.com', 'sn.im'])
-            self.assertEquals(urls, set(s['snipurl.com'].domains))
+        s = self.__parse("test/services.xml", longurl.Services)
+        self.assertEquals(109, len(s))
+        self.assertEquals('snipurl.com', s['snipurl.com'].name)
+        urls = set(['snipurl.com', 'snurl.com', 'snipr.com', 'sn.im'])
+        self.assertEquals(urls, set(s['snipurl.com'].domains))
 
 if __name__ == '__main__':
     unittest.main()
