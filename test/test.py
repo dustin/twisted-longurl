452748707974135593c46265d5047c406e82f31d
Dustin Sallings <dustin@spy.net>
2009-06-01 12:26:16 -0700

    Removed no longer used xml response parsing.

--- test/test.py
+++ test/test.py
@@ -18,29 +18,12 @@ class ParsingTest(unittest.TestCase):
         with open("../" + fn) as f:
             return parser(f.read())
 
-    def __parse_l(self, fn, parser):
-        f = open('../' + fn)
-        document=xml.dom.minidom.parseString(f.read())
-        assert document.firstChild.nodeName == "response"
-        errMsgs = document.getElementsByTagName('messages')
-        if errMsgs:
-            raise ResponseFailure(errMsgs[0].firstChild.data)
-        self.url = document.getElementsByTagName('long_url')[0].firstChild.data
-        return parser(self.url)
-
     def testServiceList(self):
         s = self.__parse("services.xml", longurl.Services)
         self.assertEquals(109, len(s))
         self.assertEquals('snipurl.com', s['snipurl.com'].name)
         urls = set(['snipurl.com', 'snurl.com', 'snipr.com', 'sn.im'])
         self.assertEquals(urls, set(s['snipurl.com'].domains))
-        # Just verifies the repr produces something.
-        self.assertTrue(repr(s))
-
-    def testExpandedURL(self):
-        s = self.__parse_l("expand.xml", longurl.ExpandedURL)
-        self.assertEquals('http://cgi.ebay.com/aw-cgi/eBayISAPI.dll?ViewItem&item=1698262135',
-                          s.url)
         # Just verifies the repr produces something.
         self.assertTrue(repr(s))
 
